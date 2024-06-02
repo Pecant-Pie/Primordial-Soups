1b2ccd7025cac3b121e8f739d1efc1660afb5a9e
Derrick Priester <47540913+Pecant-Pie@users.noreply.github.com>
2024-06-02 12:11:37 -0400

    Remove block drop from Crock

--- src/main/java/com/pecant/primordialsoups/datagen/PsLootTables.java
+++ src/main/java/com/pecant/primordialsoups/datagen/PsLootTables.java
@@ -5,6 +5,7 @@ import com.pecant.primordialsoups.Registration;
 import net.minecraft.data.loot.packs.VanillaBlockLoot;
 import net.minecraft.data.recipes.RecipeProvider;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.Blocks;
 import net.minecraftforge.registries.ForgeRegistries;
 import org.spongepowered.asm.mixin.injection.struct.InjectorGroupInfo;
 
@@ -16,7 +17,7 @@ public class PsLootTables extends VanillaBlockLoot {
 
     @Override
     protected void generate() {
-        dropSelf(Registration.CROCK_BLOCK.get());// TODO: this only exists for testing, will be removed later
+        dropOther(Registration.CROCK_BLOCK.get(), Blocks.AIR);
         dropSelf(Registration.IRON_SOUP_BLOCK.get());
         dropSelf(Registration.STOCK_BLOCK.get());
     }
