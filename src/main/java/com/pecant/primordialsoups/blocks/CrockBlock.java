69098f7ffbf058d955fd54f0200971da2ec5ea55
Derrick Priester <47540913+Pecant-Pie@users.noreply.github.com>
2024-06-05 16:02:21 -0400

    Add functionality to drain fluids from Crock

--- src/main/java/com/pecant/primordialsoups/blocks/CrockBlock.java
+++ src/main/java/com/pecant/primordialsoups/blocks/CrockBlock.java
@@ -1,11 +1,14 @@
 package com.pecant.primordialsoups.blocks;
 
 import com.pecant.primordialsoups.PrimordialSoups;
+import com.pecant.primordialsoups.Registration;
 import net.minecraft.core.BlockPos;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.world.InteractionHand;
 import net.minecraft.world.InteractionResult;
+import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.context.BlockPlaceContext;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
@@ -22,8 +25,14 @@ import net.minecraft.world.phys.shapes.BooleanOp;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
+import net.minecraftforge.common.capabilities.ForgeCapabilities;
+import net.minecraftforge.common.util.LazyOptional;
+import net.minecraftforge.fluids.capability.IFluidHandler;
+import net.minecraftforge.fluids.capability.IFluidHandlerItem;
+import net.minecraftforge.fluids.capability.templates.FluidTank;
 import net.minecraftforge.network.NetworkHooks;
 import org.jetbrains.annotations.Nullable;
+import static com.pecant.primordialsoups.PrimordialSoups.LOGGER;
 
 public class CrockBlock extends Block implements EntityBlock {
 
@@ -51,7 +60,6 @@ public class CrockBlock extends Block implements EntityBlock {
                 .strength(10F) // TODO: Make this as tough as reinforced deepslate
                 .requiresCorrectToolForDrops()
                 .sound(SoundType.METAL));
-//        this.registerDefaultState(this.stateDefinition.any().setValue(FACING, Direction.NORTH));
 
     }
 
@@ -72,65 +80,96 @@ public class CrockBlock extends Block implements EntityBlock {
     public InteractionResult use(BlockState pState, Level pLevel, BlockPos pPos, Player pPlayer, InteractionHand pHand, BlockHitResult pHit) {
 
 
-
-        if (!pLevel.isClientSide()) {
-            BlockEntity be = pLevel.getBlockEntity(pPos);
-            if (!(be instanceof CrockBlockEntity blockEntity))
-                return InteractionResult.PASS;
-            // open screen
-            NetworkHooks.openScreen((ServerPlayer) pPlayer, blockEntity, blockEntity.getBlockPos());
-            PrimordialSoups.LOGGER.debug("Open screen!");
-        }
-        return InteractionResult.SUCCESS;
-
-
-//
-//        ItemStack item = pPlayer.getMainHandItem();
-//        if (item.isEmpty())
+        ItemStack item = pPlayer.getMainHandItem();
+//        if (item.isEmpty()) { DON'T DO this until I can
 //            item = pPlayer.getOffhandItem();
-//
-//        LazyOptional<IFluidHandlerItem> fluidHandler = item.getCapability(ForgeCapabilities.FLUID_HANDLER_ITEM);
-//
-//        final boolean succ[] = {false};
-//
-//        fluidHandler.ifPresent(iFluidHandlerItem -> {
-//            if (be instanceof CrockBlockEntity crockBlockEntity) {
-//                FluidTank input = crockBlockEntity.getInputFluidTank();
-//                FluidTank output = crockBlockEntity.getOutputFluidTank();
-//                FluidTank tank = null;
-//                boolean drainItem = false;
-//                if (iFluidHandlerItem.getFluidInTank(0).isFluidEqual(Registration.STOCK_BUCKET.get().getDefaultInstance())) {
-//                    tank = crockBlockEntity.getInputFluidTank();
-//                    drainItem = true;
-//                } else if (false) {
-//                    // TODO: implement draining output fluid if not empty
-//                }
-//
-//                if (drainItem) {
-//                    int amountToDrain = tank.getSpace();
-//                    int drained = iFluidHandlerItem.drain(amountToDrain, IFluidHandler.FluidAction.SIMULATE).getAmount();
-//                    if (drained > 0 && drained == amountToDrain && tank.isFluidValid(iFluidHandlerItem.getFluidInTank(0))) {
-//                        tank.fill(iFluidHandlerItem.drain(drained, IFluidHandler.FluidAction.EXECUTE), IFluidHandler.FluidAction.EXECUTE);
-//
-//                        if (drained <= amountToDrain) {
-//                            pPlayer.setItemSlot(EquipmentSlot.MAINHAND, iFluidHandlerItem.getContainer());
-//                            PrimordialSoups.LOGGER.info("drained fluid: ", iFluidHandlerItem.getFluidInTank(0).getFluid());
-//                            succ[0] = true;
-//                        }
-//                    }
-//                }
-//                else {
-//                    // TODO: implement functionality for taking output fluid out!
-//                }
-//            }
-//        });
-//
-//        if (succ[0])
-//            return InteractionResult.sidedSuccess(pLevel.isClientSide);
-//        else {
-
 //        }
 
+        LazyOptional<IFluidHandlerItem> fluidHandler = item.getCapability(ForgeCapabilities.FLUID_HANDLER_ITEM);
+
+//        final boolean succ[] = {false};
+
+        // Could change this functionality to be more similar to the way Create does it,
+        // to allow for a player to fill empty buckets from a stack.
+        // links: https://github.com/Creators-of-Create/Create/blob/mc1.20.1/dev/src/main/java/com/simibubi/create/foundation/fluid/FluidHelper.java#L191
+        // https://github.com/Creators-of-Create/Create/blob/mc1.20.1/dev/src/main/java/com/simibubi/create/content/fluids/transfer/GenericItemFilling.java#L116
+        IFluidHandlerItem iFluidHandlerItem = fluidHandler.orElse(null);
+        if (iFluidHandlerItem != null && !pLevel.isClientSide) {
+
+            BlockEntity be = pLevel.getBlockEntity(pPos);
+            if (be instanceof CrockBlockEntity crockBlockEntity) {
+                FluidTank input = crockBlockEntity.getInputFluidTank();
+                FluidTank output = crockBlockEntity.getOutputFluidTank();
+                FluidTank tank = null;
+                boolean drainItem = false;
+                if (iFluidHandlerItem.getFluidInTank(0).isFluidEqual(Registration.STOCK_BUCKET.get().getDefaultInstance())) {
+                    tank = input;
+                    drainItem = true;
+                } else if (iFluidHandlerItem.getFluidInTank(0).isFluidEqual(output.getFluid()) ||
+                            iFluidHandlerItem.getFluidInTank(0).getAmount() == 0) {
+                    // TODO: make it so you can empty the input tank when the output tank is empty
+                    drainItem = false;
+                }
+
+                if (!drainItem && output.getSpace() < output.getCapacity())
+                    tank = output;
+                else
+                    tank = input;
+
+                if (drainItem) {
+                    LOGGER.debug("attempting to drain item");
+                    int amountToDrain = tank.getSpace();
+                    int drained = iFluidHandlerItem.drain(amountToDrain, IFluidHandler.FluidAction.SIMULATE).getAmount();
+                    LOGGER.debug("draining " + drained + "mb");
+                    if (drained > 0 && tank.isFluidValid(iFluidHandlerItem.getFluidInTank(0))) {
+                        if (pPlayer.isCreative()) {
+                            tank.fill(iFluidHandlerItem.drain(drained, IFluidHandler.FluidAction.SIMULATE), IFluidHandler.FluidAction.EXECUTE);
+                        }
+                        else {
+                            int filled = tank.fill(iFluidHandlerItem.drain(drained, IFluidHandler.FluidAction.SIMULATE), IFluidHandler.FluidAction.SIMULATE);
+                            if (drained == filled ) {
+                                tank.fill(iFluidHandlerItem.drain(drained, IFluidHandler.FluidAction.EXECUTE), IFluidHandler.FluidAction.EXECUTE);
+                                pPlayer.setItemSlot(EquipmentSlot.MAINHAND, iFluidHandlerItem.getContainer());
+                                PrimordialSoups.LOGGER.info("drained fluid: ", iFluidHandlerItem.getFluidInTank(0).getFluid());
+                            }
+                        }
+                    }
+                } else {
+                    LOGGER.info("Filling item: " + item.getDisplayName().getContents());
+                    int amountToDrain = tank.getFluidAmount();
+                    int drained = tank.drain(amountToDrain, IFluidHandler.FluidAction.SIMULATE).getAmount();
+                    if (drained > 0 && iFluidHandlerItem.isFluidValid(0, tank.getFluidInTank(0))) {
+                        if (pPlayer.isCreative()) {
+                            iFluidHandlerItem.fill(tank.drain(drained, IFluidHandler.FluidAction.EXECUTE), IFluidHandler.FluidAction.SIMULATE);
+                        }
+                        else {
+                            int filled = iFluidHandlerItem.fill(tank.drain(drained, IFluidHandler.FluidAction.SIMULATE), IFluidHandler.FluidAction.SIMULATE);
+
+                            if (filled > 0) {
+                                iFluidHandlerItem.fill(tank.drain(filled, IFluidHandler.FluidAction.EXECUTE), IFluidHandler.FluidAction.EXECUTE);
+                                pPlayer.setItemSlot(EquipmentSlot.MAINHAND, iFluidHandlerItem.getContainer());
+                            }
+                            else
+                                LOGGER.info("Failed to drain item");
+                        }
+
+                    }
+                }
+
+            }
+            return InteractionResult.sidedSuccess(pLevel.isClientSide);
+        }
+        else {
+            if (!pLevel.isClientSide()) {
+                BlockEntity be = pLevel.getBlockEntity(pPos);
+                if (!(be instanceof CrockBlockEntity blockEntity))
+                    return InteractionResult.PASS;
+                // open screen
+                NetworkHooks.openScreen((ServerPlayer) pPlayer, blockEntity, blockEntity.getBlockPos());
+                LOGGER.debug("Open screen!");
+            }
+            return InteractionResult.sidedSuccess(pLevel.isClientSide);
+        }
     }
 
     @Nullable
